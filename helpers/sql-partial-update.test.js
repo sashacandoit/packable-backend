c9612e158c862c945af68404b521e6ffa6667ae0
Sasha Czerniawski <sashacandoit@gmail.com>
2023-02-03 09:12:50 -0500

    middleware auth tests pass

--- helpers/sql-partial-update.test.js
+++ helpers/sql-partial-update.test.js
@@ -1,24 +1,24 @@
 
-const { sqlForPartialUpdate } = require("./sql-partial-update");
+const { sqlPartialUpdate } = require("./sql-partial-update");
 
 
-describe("sqlForPartialUpdate", function () {
+describe("sqlPartialUpdate", function () {
   test("works: 1 item", function () {
-    const result = sqlForPartialUpdate(
+    const result = sqlPartialUpdate(
       { f1: "v1" },
       { f1: "f1", fF2: "f2" });
     expect(result).toEqual({
-      setCols: "\"f1\"=$1",
+      setCols: "\"f1\" = $1",
       values: ["v1"],
     });
   });
 
   test("works: 2 items", function () {
-    const result = sqlForPartialUpdate(
+    const result = sqlPartialUpdate(
       { f1: "v1", jsF2: "v2" },
       { jsF2: "f2" });
     expect(result).toEqual({
-      setCols: "\"f1\"=$1, \"f2\"=$2",
+      setCols: "\"f1\" = $1, \"f2\" = $2",
       values: ["v1", "v2"],
     });
   });
